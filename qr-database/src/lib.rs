80cd15984cd9ef3b655670f2fbbc144ef9a9ea64
Mutsuha Asada <momiimt@gmail.com>
2023-02-07 22:29:56 +0900

    マイグレーションを実装

---
+++ qr-database/src/lib.rs
@@ -0,0 +1,11 @@
+use anyhow::{Context, Result};
+
+pub async fn migrate<'a, A>(conn: A) -> Result<()>
+where
+    A: sqlx::Acquire<'a, Database = sqlx::Postgres>
+{
+    sqlx::migrate!("./migrations")
+        .run(conn)
+        .await
+        .context("Failed to run migrations")
+}
