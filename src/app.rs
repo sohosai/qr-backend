b47a5ada5e14117ae26bb7442202284785c66b24
puripuri2100 <puripuri2100@gmail.com>
2023-08-11 01:01:11 +0900

    axumに乗り換え成功 exampleのとおりに書いたらきちんと動いた

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,13 @@
 use anyhow::{Context, Result};
-use sqlx::postgres::PgPool;
+use axum::{
+    extract::Json,
+    http::StatusCode,
+    routing::{get, post},
+    Router,
+};
+use sqlx::{pool::Pool, postgres::PgPool, postgres::Postgres};
 use std::net::SocketAddr;
-use warp::Filter;
+use std::sync::Arc;
 
 /// サーバーの実体
 /// データベースを起動してエントリポイントに応じて関数を呼び出す
@@ -10,51 +16,39 @@ pub async fn app(bind: SocketAddr) -> Result<()> {
         std::env::var("DATABASE_URL").context("Environment variable not set: DATABASE_URL")?;
 
     // migrateファイルを適用
-    let conn = PgPool::connect(&database_url).await?;
+    let conn = Arc::new(PgPool::connect(&database_url).await?);
     crate::database::migrate(&mut conn.acquire().await?).await?;
-    warp::serve(ping().or(insert_equipment(&conn)))
-        .run(bind)
-        .await;
+
+    // pathと関数の実体の紐づけ
+    let app = Router::new().route("/ping", get(ping)).route(
+        "/insert_equipment",
+        post({
+            let conn = Arc::clone(&conn);
+            move |body| insert_equipment(body, conn)
+        }),
+    );
+
+    // サーバーの実行
+    axum::Server::bind(&bind)
+        .serve(app.into_make_service())
+        .await?;
+
     Ok(())
 }
 
 /// ダミー
-pub fn ping() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    warp::path("ping")
-        .and(warp::get())
-        .map(|| String::from("ping"))
+pub async fn ping() -> &'static str {
+    "pong"
 }
 
 /// 備品情報の登録を行うエンドポイント
 /// - https://github.com/sohosai/qr-backend/issues/11
-pub fn insert_equipment<'a, E>(
-    conn: E,
-) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone + 'a
-where
-    E: sqlx::Executor<'a, Database = sqlx::Postgres> + Clone,
-{
-    warp::path("insert_equipment")
-        .and(warp::post())
-        .and(warp::body::json::<crate::Equipment>())
-        .and_then(
-            |equipment: crate::Equipment| async {
-                if true {
-                    Ok(String::new())
-                } else {
-                    Err(warp::reject::reject())
-                }
-            },
-            // クロージャの外部にある`conn`を参照してしまっているので
-            // ```
-            // expected a closure that implements the `Fn` trait, but this closure only implements `FnOnce`
-            // ```
-            // というエラーが出てダメ
-            // どうすれば良いのかわからず停止
-            /*async {
-                match crate::database::insert_equipment::insert_equipment(conn, equipment).await {
-                    Ok(()) => Ok(String::new()),//Ok(warp::http::StatusCode::ACCEPTED),
-                    _ => Err(warp::reject::reject()),
-                }
-            }*/
-        )
+pub async fn insert_equipment(
+    Json(equipment): Json<crate::Equipment>,
+    conn: Arc<Pool<Postgres>>,
+) -> StatusCode {
+    match crate::database::insert_equipment::insert_equipment(&*conn, equipment).await {
+        Ok(()) => StatusCode::ACCEPTED,
+        _ => StatusCode::BAD_REQUEST,
+    }
 }
