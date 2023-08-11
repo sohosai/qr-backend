3194e5319fc58e11abeb121d0b823906fbc81bb6
puripuri2100 <puripuri2100@gmail.com>
2023-08-12 06:34:58 +0900

    分割 分割はした方が良い

--- src/app.rs
+++ src/app.rs
@@ -1,13 +1,14 @@
 use anyhow::{Context, Result};
 use axum::{
-    extract::Json,
-    http::StatusCode,
     routing::{get, post},
     Router,
 };
-use sqlx::{pool::Pool, postgres::PgPool, postgres::Postgres};
+use sqlx::postgres::PgPool;
 use std::net::SocketAddr;
 use std::sync::Arc;
+
+/// 物品情報の登録を行うエンドポイントの定義
+pub mod equipment;
 
 /// サーバーの実体
 /// データベースを起動してエントリポイントに応じて関数を呼び出す
@@ -24,7 +25,7 @@ pub async fn app(bind: SocketAddr) -> Result<()> {
         "/insert_equipment",
         post({
             let conn = Arc::clone(&conn);
-            move |body| insert_equipment(body, conn)
+            move |body| equipment::insert_equipment(body, conn)
         }),
     );
 
@@ -39,16 +40,4 @@ pub async fn app(bind: SocketAddr) -> Result<()> {
 /// ダミー
 pub async fn ping() -> &'static str {
     "pong"
-}
-
-/// 備品情報の登録を行うエンドポイント
-/// - https://github.com/sohosai/qr-backend/issues/11
-pub async fn insert_equipment(
-    Json(equipment): Json<crate::Equipment>,
-    conn: Arc<Pool<Postgres>>,
-) -> StatusCode {
-    match crate::database::insert_equipment::insert_equipment(&*conn, equipment).await {
-        Ok(()) => StatusCode::ACCEPTED,
-        _ => StatusCode::BAD_REQUEST,
-    }
 }
