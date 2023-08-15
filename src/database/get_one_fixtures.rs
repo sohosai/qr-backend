18ef3bcba32e30a826fb8ac991999a5e33e44d14
puripuri2100 <puripuri2100@gmail.com>
2023-08-15 14:24:35 +0900

    add get_one_fixtures 備品の情報を検索して取得する関数を実装

--- src/database/get_one_fixtures.rs
+++ src/database/get_one_fixtures.rs
@@ -1,21 +1,21 @@
+use crate::Fixtures;
 use anyhow::{Context, Result};
 use uuid::Uuid;
 
-pub async fn delete_fixtures<'a, E>(conn: E, uuid: Uuid) -> Result<()>
+pub async fn get_one_fixtures<'a, E>(conn: E, uuid: Uuid) -> Result<Option<Fixtures>>
 where
     E: sqlx::Executor<'a, Database = sqlx::Postgres>,
 {
-    sqlx::query!("DELETE FROM fixtures WHERE id = $1", uuid)
-        .execute(conn)
+    let fixtures_opt = sqlx::query_as!(Fixtures, "SELECT * FROM fixtures WHERE id = $1", uuid)
+        .fetch_optional(conn)
         .await
-        .context("Failed to delete fixtures")?;
+        .context("Failed to get fixtures")?;
 
-    Ok(())
+    Ok(fixtures_opt)
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::database::delete_fixtures::delete_fixtures;
     use crate::database::get_one_fixtures::get_one_fixtures;
     use crate::database::insert_fixtures::insert_fixtures;
     use crate::Fixtures;
@@ -25,6 +25,7 @@ mod tests {
     #[sqlx::test(migrations = "./migrations")]
     async fn test_insert_spot_sql(pool: Pool<Postgres>) {
         let uuid = uuid!("550e8400-e29b-41d4-a716-446655440000");
+        let dummy_uuid = uuid!("550e8400-e29b-41d4-a716-446655440001");
         let info: Fixtures = serde_json::from_value(serde_json::json!({
           "id": uuid,
           "qr_id": "test",
@@ -40,11 +41,10 @@ mod tests {
         .unwrap();
 
         insert_fixtures(&pool, info).await.unwrap();
-        let result = get_one_fixtures(&pool, uuid).await.unwrap();
+        let result: Option<Fixtures> = get_one_fixtures(&pool, uuid).await.unwrap();
         assert!(result.is_some());
 
-        delete_fixtures(&pool, uuid).await.unwrap();
-        let result = get_one_fixtures(&pool, uuid).await.unwrap();
+        let result: Option<Fixtures> = get_one_fixtures(&pool, dummy_uuid).await.unwrap();
         assert!(result.is_none());
     }
 }
