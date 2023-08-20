796e98f840f8445967c8dd7d134c80f50d7d89dc
puripuri2100 <puripuri2100@gmail.com>
2023-08-21 02:54:22 +0900

    物品を検索するDB操作関数を実装 複数条件を検索するには、この関数を複数回呼び出して結果を統合することになる

---
+++ src/database/get_fixtures_list.rs
@@ -0,0 +1,77 @@
+use crate::{Fixtures, Stroge};
+use anyhow::{Context, Result};
+use uuid::Uuid;
+
+/// 検索条件
+/// クエリから上手く構築すると良い
+#[derive(Debug, Clone)]
+pub enum SelectInfo {
+    Id(Uuid),
+    QrId(String),
+    Name(String),
+    Description(String),
+    Storage(Stroge),
+    ParentId(String),
+}
+
+pub async fn get_fixtures_list<'a, E>(conn: E, select: SelectInfo) -> Result<Vec<Fixtures>>
+where
+    E: sqlx::Executor<'a, Database = sqlx::Postgres>,
+{
+    match select {
+        SelectInfo::Id(id) => {
+            let fixtures_lst =
+                sqlx::query_as!(Fixtures, "SELECT * FROM fixtures WHERE id = $1", id)
+                    .fetch_all(conn)
+                    .await
+                    .context("Failed to get fixtures")?;
+            Ok(fixtures_lst)
+        }
+        SelectInfo::QrId(id) => {
+            let fixtures_lst =
+                sqlx::query_as!(Fixtures, "SELECT * FROM fixtures WHERE qr_id = $1", id)
+                    .fetch_all(conn)
+                    .await
+                    .context("Failed to get fixtures")?;
+            Ok(fixtures_lst)
+        }
+        SelectInfo::Name(name) => {
+            let fixtures_lst =
+                sqlx::query_as!(Fixtures, "SELECT * FROM fixtures WHERE name LIKE $1", name)
+                    .fetch_all(conn)
+                    .await
+                    .context("Failed to get fixtures")?;
+            Ok(fixtures_lst)
+        }
+        SelectInfo::Description(text) => {
+            let fixtures_lst = sqlx::query_as!(
+                Fixtures,
+                "SELECT * FROM fixtures WHERE description LIKE $1",
+                text
+            )
+            .fetch_all(conn)
+            .await
+            .context("Failed to get fixtures")?;
+            Ok(fixtures_lst)
+        }
+        SelectInfo::Storage(storage) => {
+            let fixtures_lst = sqlx::query_as!(
+                Fixtures,
+                "SELECT * FROM fixtures WHERE storage = $1",
+                storage.to_string()
+            )
+            .fetch_all(conn)
+            .await
+            .context("Failed to get fixtures")?;
+            Ok(fixtures_lst)
+        }
+        SelectInfo::ParentId(id) => {
+            let fixtures_lst =
+                sqlx::query_as!(Fixtures, "SELECT * FROM fixtures WHERE parent_id = $1", id)
+                    .fetch_all(conn)
+                    .await
+                    .context("Failed to get fixtures")?;
+            Ok(fixtures_lst)
+        }
+    }
+}
